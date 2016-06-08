b1b1950d7d690141158bd5a86520b9c43d545c22
nirabhratapaswi <nirabhratapaswi@gmail.com>
2016-06-08 18:05:30 +0530

    Simple Random Rectangle Obstracles and Jumping Over Them

--- Delta Task 2/index.js
+++ Delta Task 2/index.js
@@ -1,131 +1,178 @@
-var canvas, ctx, translateXlength = 0, X = -10/3, intervalGround, gameState = 0, bounceHeight = 0, bounceDepth = 0, bounceHeightStatus = 4, bounceDepthStatus = 4, yClearance = 0, xClearance = 0, jumpStatus = 0;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var raf, jumpState = 0, ballXval = 0, ballYval = 0, startTime, timeDiff, bounceState = 0, accelarationIndex = 0,  runDist = 0, moveDistance = 10/3, gameState = 0, obstracleHeight = 30, obstracleWidth = 20;
+
+
+window.onload = function() {
+    document.getElementById("divCanvas").innerHTML = '<canvas id="canvas" width="' + screen.width + '" height="' + screen.height / 2 + '"></canvas>';
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    ball.draw();
+    ground.draw();
+}
 
 
 document.onkeydown = function(e) {
     switch (e.keyCode) {
-        case 37:
-            document.getElementById("div01").style.backgroundColor = "yellow";
-            if(gameState == 0) {
-                intervalGround = setInterval(StartGame, 100);
-                gameState = 1;
-            }
+        case 37:    //raf = window.requestAnimationFrame(draw);           //Left
             break;
-        case 38:
-            document.getElementById("div01").style.backgroundColor = "green";
-            if(bounceHeightStatus == 4 && jumpStatus == 0) {
-                bounceHeightStatus = 0;
-                jumpStatus = 1;
-            }
+        case 38:    if(jumpState == 0) {        //Up
+                        raf = window.requestAnimationFrame(drawBall);
+                        jumpState = 1;
+                        startTime = new Date();
+                        if(gameState == 0) {
+                            var startInterval = setInterval(checkHeight, 1);
+                            gameState = 1;
+                        }
+                    }
             break;
-        case 39:
-            document.getElementById("div01").style.backgroundColor = "red";
+        case 39:            //Right
             break;
-        case 40:
-            document.getElementById("div01").style.backgroundColor = "blue";
-            if(bounceDepthStatus == 4) {
-                bounceDepthStatus = 0;
-            }
+        case 40:            //Down
             break;
     }
 };
 
 
-window.onload = function() {
-    draw();
+document.onkeyup = function(e) {
+    switch (e.keyCode) {
+        case 37:            //Left
+            break;
+        case 38:            //Up
+            break;
+        case 39:            //Right
+            break;
+        case 40:            //Down
+            break;
+    }
+};
+
+
+var ball = {
+    x: screen.width/2,
+    y: screen.height/4 - 28,
+    vx: 0,
+    vy: -5,
+    ax: 0,
+    ay: 10,
+    radius: 25,
+    color: 'blue',
+    draw: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+};
+
+
+var ground = {
+    x: 100,
+    y: 100,
+    vx: 0,
+    vy: -3,
+    radius: 25,
+    color: 'blue',
+    draw: function() {
+        var i = 0;
+        ctx.beginPath();
+        ctx.moveTo(0, canvas.height / 2);
+        ctx.lineTo(canvas.width, canvas.height / 2);
+        /*for(i = 0; i < 150 + runDist; i++) {
+            ctx.moveTo(i * 10 - moveDistance, canvas.height / 2);
+            ctx.lineTo((i-1) * 10 - moveDistance, canvas.height / 2 + 10);
+            ctx.moveTo(i * 10 - moveDistance, canvas.height / 2);
+            ctx.stroke();
+        }*/
+        ctx.moveTo(0, canvas.height / 2);
+        ctx.closePath();
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+    }
+};
+
+
+function drawGround() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ground.draw();
 }
 
 
-function draw() {
-    canvas = document.getElementById("gameArea");
-    ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.save();
-    ctx.fillStyle = "blue";
-    ctx.fillRect(canvas.width/2 - 26, canvas.height/2 - 26, 25, 25);
-    ctx.moveTo(0, canvas.height/2);
-    ctx.translate(0, canvas.height/2);
-    ctx.lineTo(canvas.width, 0);
-    ctx.moveTo(0, canvas.height/2);
-    ctx.stroke();
-    var i = 0;
+function drawBall() {
+    var currentTime = new Date();
+    timeDiff = currentTime.getMilliseconds() - startTime.getMilliseconds();
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ball.draw();
+        ball.x += ball.vx;
+        accelarationIndex = (-(ball.y - (canvas.height/2 - ball.radius - 2)) - 3)/76 * 4;
+        if(bounceState == 1)
+            accelarationIndex = -accelarationIndex;
+        ball.y += ball.vy + accelarationIndex;
+        document.getElementById("p01").innerHTML = accelarationIndex;
+    ground.draw();
+    obstracle.vx -= 1;
+    obstracle.draw();
+    ctx.clearRect(canvas.width - 50 + obstracle.vx, canvas.height / 2 - 1, 2, -60);
+    if(obstracle.x <= 0) {
+        obstracle.vx = 0;
+    }
+    if(bounceState <= 1)
+        raf = window.requestAnimationFrame(drawBall);
+    else {
+        window.cancelAnimationFrame(raf);
+        bounceState = 0;
+        jumpState = 0;
+    }
+}
+
+
+ball.draw();
+
+
+var obstracle = {
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    radius: 25,
+    color: 'blue',
+    draw: function() {
+        var i = 0;
+        ctx.beginPath();
+        ctx.moveTo(canvas.width - 50 + this.vx, canvas.height / 2);
+        this.x = canvas.width - 50 + this.vx;
+        ctx.lineTo(canvas.width - 50 + this.vx, canvas.height / 2 - obstracleHeight);
+        ctx.lineTo(canvas.width - 50 - obstracleWidth + this.vx, canvas.height / 2 - obstracleHeight);
+        ctx.lineTo(canvas.width - 50 + this.vx - obstracleWidth, canvas.height / 2);
+        ctx.moveTo(canvas.width - 50 + this.vx - obstracleWidth, canvas.height / 2);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+};
+
+
+function checkHeight() {
+    if(ball.y >= (canvas.height/2 - ball.radius - 2) || ball.y <= (canvas.height/6 + ball.radius))
+    {
+        ball.vy = -ball.vy;
+        bounceState += 1;
+        ball.y += ball.vy;
+    }
     
-    for(i = 0; i < 30; i++) {
-        ctx.translate(10, 0);
-        ctx.moveTo(0, 0);
-        ctx.lineTo(-5, 10);
-        ctx.moveTo(0, 0);
-        ctx.stroke();
-        translateXlength += 10;
+    if(ball.y >= (canvas.height/2 - ball.radius - 2) || ball.y <= (canvas.height/6 + ball.radius))
+    {
+        ball.vy = -ball.vy;
+        bounceState += 1;
+        ball.y += ball.vy;
     }
-    ctx.translate(-translateXlength, 0);
-    ctx.moveTo(0, 0);
-    translateXlength = 0;
-}
-
-
-function animate() {
-    document.getElementById("div01").innerHTML = jumpStatus;
-    ctx.restore();
-    ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.save();
-    ctx.fillStyle = "blue";
-    ctx.fillRect(canvas.width/2 - 26, canvas.height/2 - 26 + bounceHeight + bounceDepth, 25, 25);
-    ctx.moveTo(0, canvas.height/2);
-    ctx.translate(0, canvas.height/2);
-    ctx.lineTo(canvas.width, 0);
-    ctx.moveTo(0, canvas.height/2);
-    ctx.stroke();
-    var i = 0;
-    ctx.translate(X, 0);
-    for(i = 0; i < 31; i++) {
-        ctx.translate(10, 0);
-        ctx.moveTo(0, 0);
-        ctx.lineTo(-5, 10);
-        ctx.moveTo(0, 0);
-        ctx.stroke();
-        translateXlength += 10;
+    obstracle.vx -= 1;
+    obstracle.draw();
+    ctx.clearRect(canvas.width - 50 + obstracle.vx, canvas.height / 2 - 1, 1, -obstracleHeight - 1);
+    if(obstracle.x <= 0) {
+        obstracle.vx = 0;
+        obstracleHeight = Math.ceil(Math.random() * 20) + 30;
+        obstracleWidth = Math.ceil(Math.random() * 20) + 20;
     }
-    ctx.translate(-translateXlength, 0);
-    ctx.moveTo(0, 0);
-    translateXlength = 0;
-    if(X == -10/3)
-        X = -20/3;
-    else if(X == -20/3)
-        X = -10;
-    else if(X == -10)
-        X = -10/3;
-    if(bounceHeightStatus <= 3) {
-        bounceHeight -= 5;
-        yClearance += 5;
-        bounceHeightStatus += 1;
-    }
-    else {
-        bounceHeightStatus = 4;
-        bounceDepthStatus = 0;
-    }
-    if(bounceDepthStatus <= 3 && yClearance > 0) {
-        bounceDepth += 5;
-        yClearance -= 5;
-        bounceDepthStatus += 1;
-    }
-    else {
-        bounceDepthStatus = 4;
-    }
-    if(yClearance <= 0)
-        jumpStatus = 0;
-}
-
-
-function StartGame() {
-    ctx.clearRect(0, -canvas.height/2, canvas.width*2, canvas.height);
-    ctx.closePath();
-    animate();
-}
-
-
-function bounceUp() {
-    ctx.fillStyle = "blue";
-    ctx.fillRect(canvas.width/2 - 36, canvas.height/2 - 36, 25, 25);
 }
